faac6b4ae0a11b17776af56108bb897a94b5727a
Chongyang-Cao <caoc@stpaulsschool.org.uk>
2022-10-13 15:40:58 +0100

    done

---
+++ 7516/Program.cs
@@ -0,0 +1,23 @@
+﻿static double HCF(double number1, double number2)
+{
+    double temp1 = number1;
+    double temp2 = number2;
+    while (temp1 != temp2)
+    {
+        if (temp1 > temp2)
+        {
+            temp1 = temp1 - temp2;
+        }
+        else
+        {
+            temp2 = temp2 - temp1;
+        }
+    }
+    return temp1;
+}
+Console.WriteLine("Enter a whole number");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter another whole number");
+double number2 = Convert.ToDouble(Console.ReadLine());
+double result = HCF(number1, number2);
+Console.WriteLine($"{result} is the HCF of {number1} and {number2}");
---
+++ 7517/Program.cs
@@ -0,0 +1,28 @@
+﻿static void cnvBinary1(int a)
+{
+    while (a != 0)
+    {
+        Console.WriteLine(a%2);
+        a = a / 2;
+    }
+}
+//Console.WriteLine("What number to turn into binary?");
+//int a = Convert.ToInt32(Console.ReadLine());
+//cnvBinary1(a);
+
+static void cnvBinary2(int a)
+{
+    List<int> list = new List<int>();
+    while (a != 0)
+    {
+        list.Add(a % 2);
+        a = a / 2;
+        //Console.WriteLine(a%2);
+        //Console.WriteLine(list.Count);
+    }
+    list.Reverse();
+    Console.WriteLine(string.Join("", list));
+}
+//Console.WriteLine("What number to turn into binary?");
+//int b = Convert.ToInt32(Console.ReadLine());
+//cnvBinary2(b);
